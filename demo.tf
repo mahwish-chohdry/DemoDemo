40d43dd3a374db8e420c2de111dd437f8f75cbf6
Mahwish Chohdry <mahwish@xavor.com>
2021-08-12 10:32:37 +0500

    my demo

---
+++ demo.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.69.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features{}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "RG-TF-Testing"
+  location = "eastus"
+  }
+
+  resource "azurerm_virtual_network" "vnet" {
+  name                = "VNet-TF-Testing"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+  }

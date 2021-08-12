8b935ebb37f0aa0b43cb96aeb9925099f006d335
Mahwish Chohdry <mahwish@xavor.com>
2021-08-12 11:40:56 +0500

    e

--- demo.tf
+++ demo.tf
@@ -11,14 +11,7 @@ provider "azurerm" {
   # Configuration options
   features{}
 }
-terraform {
-backend "azurerm"{
-  resource_group_name  = "tfstate-rg"
-  storage_account_name = "fmahwisht"
-  container_name   = "tfstate"
-  key  =  "dev.terraform.tfstate"
-  }
-}
+
 resource "azurerm_resource_group" "rg" {
   name     = "RG-TF-Testing"
   location = "eastus"
